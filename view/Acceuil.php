60fcd0ebfeca24dfd16a1e79e2d865502231453f
Ahmad Daoud <115790541+Ahmad-Daoud@users.noreply.github.com>
2023-07-02 21:40:13 +0200

    s

--- view/Acceuil.php
+++ view/Acceuil.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bienvenue à Tempterest</title>
-    <link rel="icon" type="image/x-icon" href="https://adaoud.dev/Tempterest/TempTerest/logo.ico">s
+    <link rel="icon" type="image/x-icon" href="https://adaoud.dev/Tempterest/TempTerest/logo.ico">
 </head>
 <body>
     Bienvenu(e) à l'acceuil
