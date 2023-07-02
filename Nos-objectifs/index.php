80d4ff725f9cb7c0f82daee51bbc18f15c869e52
Ahmad Daoud <115790541+Ahmad-Daoud@users.noreply.github.com>
2023-07-02 17:52:31 +0200

    Update index.php

--- Nos-objectifs/index.php
+++ Nos-objectifs/index.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Les objectifs de Tempterest</title>
     <link rel="stylesheet" href="https://adaoud.dev/Tempterest/TempTerest/css-layout/objectifs.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Carter+One&family=Josefin+Sans:wght@700&family=Pacifico&display=swap" rel="stylesheet">
 </head>
 <body>
     <?php
