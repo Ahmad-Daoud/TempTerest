40409abc3304c0778ffaea4a0897e3b58cbe4db0
Ahmad Daoud <115790541+Ahmad-Daoud@users.noreply.github.com>
2023-07-02 21:16:04 +0200

    Update index.php

--- Nos-modeles/modele/index.php
+++ Nos-modeles/modele/index.php
@@ -14,11 +14,16 @@
             require("../../view/login.php");
         }
         Function DisplayPage(){
-            $id = $_GET["Id"];
-            global $db;
-            echo"$id";
-            $query1 = "SELECT * FROM models_details WHERE id LIKE $id";
-            $query2 = "SELECT * FROM models_zones WHERE model_id LIKE $id";
+            if (isset($_GET["Id"])){
+                $id = $_GET["Id"];
+                global $db;
+                echo"$id";
+                $query1 = "SELECT * FROM models_details WHERE id LIKE $id";
+                $query2 = "SELECT * FROM models_zones WHERE model_id LIKE $id";
+            }
+            else {
+                echo'no id chosen';
+            }
             ?> 
 
                 This is an html portion of the page
