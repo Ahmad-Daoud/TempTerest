c7f911857004798718b59d20d9c646405d0fa4ad
Ahmad Daoud <115790541+Ahmad-Daoud@users.noreply.github.com>
2023-07-08 20:09:41 +0200

    Update index.php

--- Nos-modeles/modele/index.php
+++ Nos-modeles/modele/index.php
@@ -89,7 +89,7 @@
                             }
                             $html_bf = $row["html_code"];
                             $num = find_highest_zone($row["html_code"]);
-                            echo $html_bf;
+                            echo $html_bf . 'lol';
                             $html_added = addZoneDetails($html_bf);
                             $html = convertHTMLSigns($html_added);
                             $css = $row["css_code"];
