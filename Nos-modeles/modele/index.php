cf7c102aa902f264ee947950915f118c9a8a1684
Ahmad Daoud <115790541+Ahmad-Daoud@users.noreply.github.com>
2023-07-08 18:24:11 +0200

    Update index.php

--- Nos-modeles/modele/index.php
+++ Nos-modeles/modele/index.php
@@ -27,6 +27,12 @@
             }
             return $highestZone;
         }
+        function convertHTMLSigns($html) {
+            $search = array('&', '<', '>', '"');
+            $replace = array('&amp;', '&lt;', '&gt;', '&quot;');
+            $converted = str_replace($search, $replace, $html);
+            return $converted;
+        }
         function DisplayPage(){
 
             if (isset($_GET["Id"])){
@@ -47,7 +53,7 @@
                         $result2 = $db->query($query2);
                         while ($row = $result2->fetch_assoc()) {
                             $num = find_highest_zone($row["html_code"]);
-                            $html = $row["html_code"];
+                            $html = convertHTMLSigns($row["html_code"]);
                         }
                         // l'utilisateur a déjà choisi les paramètres de son modèle
                         if (isset($_POST["div1"])){
